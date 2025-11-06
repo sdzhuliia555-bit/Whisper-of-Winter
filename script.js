bede47799b28700c17c1ed644ca0f7ba681bf7a2
Dzhuliia <s.dzhuliia555@gmail.com>
2025-11-06 15:45:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+// Простая анимация лёгкого движения животных при скролле
+document.addEventListener("scroll", () => {
+  const images = document.querySelectorAll(".gallery img");
+  const scrollY = window.scrollY;
+  images.forEach((img, index) => {
+    const speed = (index + 1) * 0.2;
+    img.style.transform = translateY(${scrollY * speed * 0.05}px);
+  });
+});
