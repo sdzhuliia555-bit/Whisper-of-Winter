6ad2942f93161abb43c8d1ce0339f4a8f4f67fd8
Dzhuliia <s.dzhuliia555@gmail.com>
2025-11-06 16:21:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const overlay = document.querySelector('.overlay');
-
 let flicker = 0;
 
 function candleFlicker() {
   flicker += 0.02;
   const opacity = 0.14 + Math.sin(flicker) * 0.03;
-  overlay.style.background = radial-gradient(circle at 50% 20%, rgba(255, 215, 130, ${opacity}), rgba(10, 5, 0, 0.85) 80%);
+  overlay.style.background = radial-gradient(circle at 50% 20%, rgba(255, 215, 130, ${opacity}), rgba(5, 5, 0, 0.85) 80%);
   requestAnimationFrame(candleFlicker);
 }
 
